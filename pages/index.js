d14a8afa8ffa1747f7427cdd1c233cf77851bf6e
Hugo Pierre <28348310+hugopierre@users.noreply.github.com>
2024-01-11 18:22:38 +0000

    Altera o conteúdo da página

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Hugo, parabéns pelo esforço! Aqui é nós por nós!</h1>
+    return <h1>Hugo, parabéns pelo esforço! Aqui é nós por nós! 💪🏻</h1>
 }
 
 export default Home;
