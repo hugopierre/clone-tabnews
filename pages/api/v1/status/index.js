0b278436b56488cb8594089a7eac9f8877d7399d
Hugo Pierre <28348310+hugopierre@users.noreply.github.com>
2025-11-10 18:19:22 +0000

    adiciona os arquivos `database.js` e `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  response.status(200).json('{"chave": "valor"}');
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 2 + 0 as sum;");
+  console.log(result.rows);
+  response.status(200).json({ status: "ok", result: result.rows });
 }
 
 export default status;
