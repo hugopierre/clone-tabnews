056a1958823e63656cc66756a124ba9d0f5401b0
Hugo Pierre <28348310+hugopierre@users.noreply.github.com>
2025-11-11 16:26:40 +0000

    adiciona a data de atualizacao no endpoint `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,10 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 2 + 0 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ status: "ok", result: result.rows });
+  const updatedAt = new Date().toISOString();
+  response.status(200).json({
+    updated_at: updatedAt,
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,11 @@
 test("GET to /api/v1/status should return 200)", async () => {
-  response = await fetch("http://localhost:3000/api/v1/status");
+  const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(parseUpdatedAt).toEqual(responseBody.updated_at);
 });
